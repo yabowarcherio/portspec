30b049d763abf297eb79b02c4d9d0be9be0aa0de
yabowarcherio <yabowarcher8590@gmail.com>
2026-06-27 11:18:42 +0300

    bench: add micro-benches for presets, TaggedSpec parsing, nth_port

--- benches/spec.rs
+++ benches/spec.rs
@@ -29,5 +29,38 @@ fn bench_set_ops(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_parse, bench_contains, bench_set_ops);
+fn bench_presets(c: &mut Criterion) {
+    use portspec::{top_100_tcp, top_1000_tcp};
+    c.bench_function("top_100_tcp parse", |b| b.iter(top_100_tcp));
+    c.bench_function("top_1000_tcp parse", |b| b.iter(top_1000_tcp));
+    let small = top_100_tcp();
+    c.bench_function("contains in top-100", |b| {
+        b.iter(|| black_box(&small).contains(black_box(443)))
+    });
+}
+
+fn bench_tagged(c: &mut Criterion) {
+    use portspec::TaggedSpec;
+    let spec = "T:22,80,443,U:53,67-68,123,514";
+    c.bench_function("parse tagged spec", |b| {
+        b.iter(|| black_box(spec).parse::<TaggedSpec>().unwrap())
+    });
+}
+
+fn bench_nth_port(c: &mut Criterion) {
+    let spec: PortSpec = "1-1024,8000-9000,30000-40000".parse().unwrap();
+    c.bench_function("nth_port middle", |b| {
+        b.iter(|| black_box(&spec).nth_port(black_box(5000)))
+    });
+}
+
+criterion_group!(
+    benches,
+    bench_parse,
+    bench_contains,
+    bench_set_ops,
+    bench_presets,
+    bench_tagged,
+    bench_nth_port,
+);
 criterion_main!(benches);
